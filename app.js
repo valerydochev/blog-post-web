890997e0c7ba09aae654f1489034012ee3ebdb33
valerydochev <valerisdochev@gmail.com>
2024-09-29 12:10:50 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import router  from "./server/routes/main.js";
+import expressLayout from "express-ejs-layouts";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.static("public"));
+
+
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+
+app.use("/", router);
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
