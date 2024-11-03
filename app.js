e2f9ce37851e3c8a891cce1dc41a40bd0bfb5a2f
valerydochev <valerisdochev@gmail.com>
2024-11-03 22:17:23 +0200

    ...

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 const PORT = process.env.PORT;
   
 connectDB();
+   
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use(express.static("public"));
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -52,6 +52,30 @@ router.get("/post/:id", async (req, res) => {
     }
 
 });
+  
+router.post("/search", async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Blog page created with NodeJs, Express & MongoDb."
+        };
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+                { body: { $regex: new RegExp(searchNoSpecialChar, "i") } }
+            ]
+        });
+
+        res.render("search", { data, locals });
+
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
   
